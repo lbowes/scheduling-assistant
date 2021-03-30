2d44fa26d12acd6ee0f4000091618cbc1f441c4c
Lewis Bowes <33807016+lbowes@users.noreply.github.com>
2021-03-30 08:29:34 +0100

    Add file io tests

--- tests/test_scheduling_assistant.py
+++ tests/test_scheduling_assistant.py
@@ -1,5 +1,10 @@
 import pytest
-from typing import Dict
+import subprocess
+import os
+import json
+from typing import Dict, List
+from random import shuffle
+from faker.providers.lorem.en_US import Provider
 
 from action_calculation import calculate_action
 
@@ -52,3 +57,52 @@ def test_action_calculation() -> None:
     target_alloc = { "C": 0.5, "D": 0.5 }
     future = calculate_action(current_time_spent_s, target_alloc)
     assert future == { "allocation": target_alloc }
+
+
+def random_json_file_names(tmp_path, faker, count) -> List[str]:
+    random_words = list(set(Provider.word_list))
+    shuffle(random_words)
+
+    return [tmp_path / str(w + ".json") for w in random_words[0:count]]
+
+
+def test_script_incomplete_args(tmp_path, faker) -> None:
+    history_file = random_json_file_names(tmp_path, faker, 1)[0]
+
+    with open(history_file, 'w', encoding='utf-8') as f:
+        json.dump({}, f, ensure_ascii=False, indent=4)
+
+    # Incomplete arguments
+    cmd = "python action_calculation.py --history \"{0}\""
+    exit_status = os.system(cmd.format(history_file))
+    assert exit_status != 0
+    
+
+def test_script_complete_args(tmp_path, faker) -> None:
+    history_file, target_alloc_file, action_output_file = random_json_file_names(tmp_path, faker, 3)
+
+    current_time_spent_s = { "A": 2 }
+    with open(history_file, 'w', encoding='utf-8') as f:
+        json.dump(current_time_spent_s, f, ensure_ascii=False, indent=4)
+
+    target_alloc = { "A": 1, "B": 1 }
+    with open(target_alloc_file, 'w', encoding='utf-8') as f:
+        json.dump(target_alloc, f, ensure_ascii=False, indent=4)
+
+    # Complete arguments
+    cmd = "python action_calculation.py --history \"{0}\" --target \"{1}\"".format(history_file, target_alloc_file)
+    exit_status = os.system(cmd)
+    assert exit_status == 0
+
+    # With output file
+    cmd += " --output \"{0}\"".format(action_output_file)
+    exit_status = os.system(cmd)
+    assert exit_status == 0
+
+    # Check output file
+    assert os.path.exists(action_output_file)
+
+    with open(action_output_file, 'r') as f:
+        action = json.load(f)
+
+    assert action == { "allocation": { "B": 1.0 }, "min_required_time_s": 2 }
