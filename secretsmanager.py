ef02e6f94209975c75ad2d0d3613c2b51bd18303
Lewis Bowes <33807016+lbowes@users.noreply.github.com>
2021-04-25 16:36:26 +0100

    Print get secret request full exception

--- secretsmanager.py
+++ secretsmanager.py
@@ -17,35 +17,31 @@ class SecretsManager:
     def get_secret(self, name: str) -> str:
         try:
             get_secret_value_response = self._client.get_secret_value(SecretId=name)
-            print("get_secret_value_response: " + str(get_secret_value_response))
         except ClientError as e:
             error_code = e.response['Error']['Code'] 
-            print("error_code: " + str(error_code))
-
+            
+            if error_code == 'AccessDeniedException':
+                print(str(e.reponse))
+                raise e
             if error_code == 'DecryptionFailureException':
                 # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
                 # Deal with the exception here, and/or rethrow at your discretion.
-                print('DecryptionFailureException')
                 raise e
             elif error_code == 'InternalServiceErrorException':
                 # An error occurred on the server side.
                 # Deal with the exception here, and/or rethrow at your discretion.
-                print('InternalServiceErrorException')
                 raise e
             elif error_code == 'InvalidParameterException':
                 # You provided an invalid value for a parameter.
                 # Deal with the exception here, and/or rethrow at your discretion.
-                print('InvalidParameterException')
                 raise e
             elif error_code == 'InvalidRequestException':
                 # You provided a parameter value that is not valid for the current state of the resource.
                 # Deal with the exception here, and/or rethrow at your discretion.
-                print('InvalidRequestException')
                 raise e
             elif error_code == 'ResourceNotFoundException':
                 # We can't find the resource that you asked for.
                 # Deal with the exception here, and/or rethrow at your discretion.
-                print('ResourceNotFoundException')
                 raise e
         else:
             # Decrypts secret using the associated KMS CMK.
