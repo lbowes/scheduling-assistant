dfd991b84c0a2e3643c09c9a7d2a58aa5fb712a1
Lewis Bowes <33807016+lbowes@users.noreply.github.com>
2021-05-07 23:04:06 +0100

    Intial work on algorithm redesign
    Dependent on order in which activities are completed, no-longer analytical.
    Also, removed old test data files

--- logic.py
+++ logic.py
@@ -1,48 +1,111 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
+from itertools import cycle
 
 
-def calc_future_alloc(current_time_spent_s: Dict[str, int], target_alloc_scores: Dict[str, int]) -> Dict[str, any]:
-    """Calculate the percentage allocation of future time between a set of activities, given a target allocation between
-    them and the current amount of time spent on each."""
-    total_score = sum(target_alloc_scores.values())
-    target_alloc = { act: float(score) / total_score for act, score in target_alloc_scores.items() if score > 0 }
+#def calc_future_alloc(current_time_spent_s: Dict[str, int], target_alloc_scores: Dict[str, int]) -> Dict[str, any]:
+#    """Calculate the percentage allocation of future time between a set of activities, given a target allocation between
+#    them and the current amount of time spent on each."""
+#    total_score = sum(target_alloc_scores.values())
+#    target_alloc = { act: float(score) / total_score for act, score in target_alloc_scores.items() if score > 0 }
+#
+#    future_alloc = { "allocation": target_alloc }
+#
+#    if not current_time_spent_s:
+#        return future_alloc
+#
+#    for target_act in target_alloc:
+#        if target_act not in current_time_spent_s:
+#            current_time_spent_s[target_act] = 0
+#
+#    relevant_time_spent_s = sum(current_time_spent_s[act] for act in target_alloc)
+#
+#    time_required_s = 0
+#
+#    for act, target in target_alloc.items():
+#        required_time_s = (current_time_spent_s[act] / target_alloc[act]) - relevant_time_spent_s
+#
+#        if required_time_s > time_required_s:
+#            time_required_s = required_time_s
+#
+#    if time_required_s <= 0:
+#        return future_alloc
+#
+#    future_alloc["min_required_time_s"] = time_required_s
+#    future_alloc["allocation"] = {}
+#
+#    total_time_s = relevant_time_spent_s + time_required_s
+#
+#    for act, target in target_alloc.items():
+#        target_alloc[act]
+#        total_time_s
+#        current_time_spent_s[act]
+#        time_required_s
+#
+#        allocation = (target_alloc[act] * total_time_s - current_time_spent_s[act]) / time_required_s
+#
+#        if allocation:
+#            future_alloc["allocation"][act] = allocation
+#
+#    return future_alloc
 
-    future_alloc = { "allocation": target_alloc }
 
-    if not current_time_spent_s:
-        return future_alloc
+def prioritize(goal: Dict[str, int], past: List[Tuple[str, int]]) -> None:
+    if not goal:
+        return None
 
-    for target_act in target_alloc:
-        if target_act not in current_time_spent_s:
-            current_time_spent_s[target_act] = 0
+    # This gives the order in which to complete activities
+    sorted_activities = [name for name, score in sorted(goal.items(), key=lambda x: x[1], reverse=True)]
+    activity_cycle = cycle(sorted_activities)
 
-    relevant_time_spent_s = sum(current_time_spent_s[act] for act in target_alloc)
+    total_score = sum(goal.values())
+    goal_percent  = { act: float(score) / total_score for act, score in goal.items() if score > 0 }
 
-    time_required_s = 0
+    current_time_spent_s = dict.fromkeys(goal.keys(), 0)
+    time_required_s = current_time_spent_s.copy()
+    total_time_s = 0
+    priority = next(activity_cycle)
 
-    for act, target in target_alloc.items():
-        required_time_s = (current_time_spent_s[act] / target_alloc[act]) - relevant_time_spent_s
+    first_iteration = True
 
-        if required_time_s > time_required_s:
-            time_required_s = required_time_s
+    for activity, dur_s in past:
+        if activity not in goal:
+            continue
 
-    if time_required_s <= 0:
-        return future_alloc
+        current_time_spent_s[activity] += dur_s
+        total_time_s += dur_s
 
-    future_alloc["min_required_time_s"] = time_required_s
-    future_alloc["allocation"] = {}
+        if current_time_spent_s[priority] >= time_required_s[priority] and not first_iteration:
+            priority = next(activity_cycle)
 
-    total_time_s = relevant_time_spent_s + time_required_s
+        for act, time_spent_s in current_time_spent_s.items():
+            if time_spent_s > goal_percent[act] * total_time_s:
+                print("completed act " + act + " with goal percent " + str(goal_percent[act]) + " after spending " +
+                        str(time_spent_s) + " out of " + str(goal_percent[act] * total_time_s))
+                completed_act = act
 
-    for act, target in target_alloc.items():
-        target_alloc[act]
-        total_time_s
-        current_time_spent_s[act]
-        time_required_s
+        for act, time_spent_s in current_time_spent_s.items():
+            future_time_spent_s = current_time_spent_s[completed_act] * (goal_percent[act] / goal_percent[completed_act])
+            time_required_s[act] = abs(future_time_spent_s - time_spent_s)
 
-        allocation = (target_alloc[act] * total_time_s - current_time_spent_s[act]) / time_required_s
+        print("\n\n\n")
+        first_iteration = False
 
-        if allocation:
-            future_alloc["allocation"][act] = allocation
+    output = []
 
-    return future_alloc
+    start_idx = sorted_activities.index(priority)
+    num_activities = len(goal)
+    for i in range(num_activities):
+        activity = sorted_activities[(start_idx + i) % num_activities]
+
+        output.append((activity, time_required_s[activity]))
+
+    return output
+
+
+if __name__ == '__main__':
+    goal = { "A": 1, "B": 1, "C": 1, "D": 1, "E": 1, "F": 1}
+    
+    past = [ ("B", 1), ("A", 20), ("B", 20), ("C", 10) ]
+    future = prioritize(goal, past)
+    print("future: " + str(future))
+    assert future == [ ("C", 11), ("D", 21), ("E", 21), ("F", 21), ("A", 1), ("B", 0) ]
--- tests/test_scheduling_assistant.py
+++ tests/test_scheduling_assistant.py
@@ -1,53 +1,174 @@
 import pytest
 
-from logic import calc_future_alloc
+from logic import prioritize
 
 
-def test_calc_future_alloc() -> None:
-    # ==================== Normal input ====================
-    # Case 1
-    current_time_spent_s = { "A": 2 }
-    target_alloc = { "A": 1, "B": 1 }
-    future = calc_future_alloc(current_time_spent_s, target_alloc)
-    assert future == { "allocation": { "B": 1.0 }, "min_required_time_s": 2 }
+#def test_calc_future_alloc() -> None:
+#    # ==================== Normal input ====================
+#    # Case 1
+#    current_time_spent_s = { "A": 2 }
+#    target_alloc = { "A": 1, "B": 1 }
+#    future = calc_future_alloc(current_time_spent_s, target_alloc)
+#    assert future == { "allocation": { "B": 1.0 }, "min_required_time_s": 2 }
+#
+#    # Case 2
+#    current_time_spent_s = { "C": 8, "D": 2 }
+#    target_alloc = { "C": 9, "D": 1 }
+#    future = calc_future_alloc(current_time_spent_s, target_alloc)
+#    assert future == { "allocation": { "C": 1.0 }, "min_required_time_s": 10 }
+#
+#    # Case 3
+#    current_time_spent_s = { "A": 3, "B": 2, "C": 5 }
+#    target_alloc = { "A": 0.2, "B": 0.4, "C": 0.4 }
+#    future = calc_future_alloc(current_time_spent_s, target_alloc)
+#    assert future == { "allocation": { "B": 0.8, "C": 0.2 }, "min_required_time_s": 5 }
+#
+#    # Case 4
+#    current_time_spent_s = { "C": 8, "D": 2 }
+#    target_alloc = { "C": 0.9, "D": 0.1 }
+#    future = calc_future_alloc(current_time_spent_s, target_alloc)
+#    assert future == { "allocation": { "C": 1.0 }, "min_required_time_s": 10 }
+#
+#    # Case 5
+#    current_time_spent_s = { "A": 1, "B": 2, "C": 3 }
+#    target_alloc = { "B": 0.25, "C": 0.75 }
+#    future = calc_future_alloc(current_time_spent_s, target_alloc)
+#    assert future == { "allocation": { "C": 1.0 }, "min_required_time_s": 3 }
+#
+#    # ==================== No past activities ====================
+#    current_time_spent_s = {}
+#    target_alloc = { "C": 0.5, "D": 0.5 }
+#    future = calc_future_alloc(current_time_spent_s, target_alloc)
+#    assert future == { "allocation": target_alloc }
+#
+#    current_time_spent_s = None
+#    target_alloc = { "C": 0.5, "D": 0.5 }
+#    future = calc_future_alloc(current_time_spent_s, target_alloc)
+#    assert future == { "allocation": target_alloc }
+#
+#    # ==================== No relevant past activities ====================
+#    current_time_spent_s = { "A": 2 }
+#    target_alloc = { "C": 0.5, "D": 0.5 }
+#    future = calc_future_alloc(current_time_spent_s, target_alloc)
+#    assert future == { "allocation": target_alloc }
 
-    # Case 2
-    current_time_spent_s = { "C": 8, "D": 2 }
-    target_alloc = { "C": 9, "D": 1 }
-    future = calc_future_alloc(current_time_spent_s, target_alloc)
-    assert future == { "allocation": { "C": 1.0 }, "min_required_time_s": 10 }
 
-    # Case 3
-    current_time_spent_s = { "A": 3, "B": 2, "C": 5 }
-    target_alloc = { "A": 0.2, "B": 0.4, "C": 0.4 }
-    future = calc_future_alloc(current_time_spent_s, target_alloc)
-    assert future == { "allocation": { "B": 0.8, "C": 0.2 }, "min_required_time_s": 5 }
+def test_prioritize_no_target() -> None:
+    goal = {}
+    past = []
+    future = prioritize(goal, past)
+    assert future == None
 
-    # Case 4
-    current_time_spent_s = { "C": 8, "D": 2 }
-    target_alloc = { "C": 0.9, "D": 0.1 }
-    future = calc_future_alloc(current_time_spent_s, target_alloc)
-    assert future == { "allocation": { "C": 1.0 }, "min_required_time_s": 10 }
 
-    # Case 5
-    current_time_spent_s = { "A": 1, "B": 2, "C": 3 }
-    target_alloc = { "B": 0.25, "C": 0.75 }
-    future = calc_future_alloc(current_time_spent_s, target_alloc)
-    assert future == { "allocation": { "C": 1.0 }, "min_required_time_s": 3 }
+def test_prioritize_no_past() -> None:
+    past = []
 
-    # ==================== No past activities ====================
-    current_time_spent_s = {}
-    target_alloc = { "C": 0.5, "D": 0.5 }
-    future = calc_future_alloc(current_time_spent_s, target_alloc)
-    assert future == { "allocation": target_alloc }
+    goal = { "A": 3, "B": 2 }
+    future = prioritize(goal, past)
+    assert future == [ ("A", 0), ("B", 0) ]
 
-    current_time_spent_s = None
-    target_alloc = { "C": 0.5, "D": 0.5 }
-    future = calc_future_alloc(current_time_spent_s, target_alloc)
-    assert future == { "allocation": target_alloc }
+    goal = { "X": 4, "Y": 5 }
+    future = prioritize(goal, past)
+    assert future == [ ("Y", 0), ("X", 0) ]
 
-    # ==================== No relevant past activities ====================
-    current_time_spent_s = { "A": 2 }
-    target_alloc = { "C": 0.5, "D": 0.5 }
-    future = calc_future_alloc(current_time_spent_s, target_alloc)
-    assert future == { "allocation": target_alloc }
+    goal = { "P": 7 }
+    future = prioritize(goal, past)
+    assert future == [ ("P", 0) ]
+
+
+def test_prioritize_equal() -> None:
+    goal = { "A": 1, "B": 1 }
+
+    past = [ ("A", 2), ("B", 1) ]
+    future = prioritize(goal, past)
+    assert future == [ ("B", 1), ("A", 0) ]
+
+
+def test_prioritize_not_started() -> None:
+    goal = { "A": 1, "B": 1, "C": 1, "D": 1, "E": 1, "F": 1}
+    
+    past = [ ("B", 1), ("A", 20), ("B", 20), ("C", 10) ]
+    future = prioritize(goal, past)
+    print("future: " + str(future))
+    assert future == [ ("C", 11), ("D", 21), ("E", 21), ("F", 21), ("A", 1), ("B", 0) ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Priority:            A
+#Processing activity: B
+#Duration:            1
+#Current time spent:  {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0}
+#Time required:       {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0}
+#Switching priority to B
+#
+#
+#Priority:            B
+#Processing activity: A
+#Duration:            20
+#Current time spent:  {'A': 0, 'B': 1, 'C': 0, 'D': 0, 'E': 0, 'F': 0}
+#Time required:       {'A': 1.0, 'B': 0.0, 'C': 1.0, 'D': 1.0, 'E': 1.0, 'F': 1.0}
+#Switching priority to C
+#
+#
+#Priority:            C
+#Processing activity: B
+#Duration:            20
+#Current time spent:  {'A': 20, 'B': 1, 'C': 0, 'D': 0, 'E': 0, 'F': 0}
+#Time required:       {'A': 0.0, 'B': 19.0, 'C': 20.0, 'D': 20.0, 'E': 20.0, 'F': 20.0}
+#
+#
+#Priority:            C
+#Processing activity: C
+#Duration:            10
+#Current time spent:  {'A': 20, 'B': 21, 'C': 0, 'D': 0, 'E': 0, 'F': 0}
+#Time required:       {'A': 1.0, 'B': 0.0, 'C': 21.0, 'D': 21.0, 'E': 21.0, 'F': 21.0}
